c54617a7aad24f33fa12cea35793fe914fab9146
Akbarjon001 <Akbarjonsolihov1@gmail.com>
2025-08-14 17:38:44 +0900

    Update For 14.08.2025-17-38

--- app/db/functions/users.py
+++ app/db/functions/users.py
@@ -10,6 +10,15 @@ async def save_user(session: AsyncSession, user_id: int, username: str | None, r
     if ref == "":
         ref = None
 
+    if ref:
+        ref_user = await session.execute(
+            select(User).where(User.ref_code == ref)
+        )
+        ref_user = ref_user.scalars().first()
+        if not ref_user:
+            ref = None
+
+
     user = User(
         user_id=user_id,
         ref_code=await generate_random_key(length=8),
@@ -31,12 +40,29 @@ async def get_user_by_id(session: AsyncSession, user_id: int) -> User:
     return user
 
 
-async def update_user_balance(session: AsyncSession, user_id: int, amount: int) -> User:
+async def update_user_balance(session: AsyncSession, user_id: int, amount: int, increase: bool = True) -> User:
     user = await get_user_by_id(session, user_id)
     if not user:
         return None
 
-    user.balance += amount
+    if increase:
+        user.balance += amount
+    else:
+        user.balance -= amount
     await session.commit()
     await session.refresh(user)
     return user.balance
+
+
+async def update_user_adv_balance(session: AsyncSession, user_id: int, amount: int, increase: bool = True) -> User:
+    user = await get_user_by_id(session, user_id)
+    if not user:
+        return None
+
+    if increase:
+        user.adv_balance += amount
+    else:
+        user.adv_balance -= amount
+    await session.commit()
+    await session.refresh(user)
+    return user.adv_balance
