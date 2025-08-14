14a8769d6741a5b427fa5668b02e60ec75e1182a
Akbarjon001 <Akbarjonsolihov1@gmail.com>
2025-08-14 17:54:31 +0900

    Update For 14.08.2025-17-54

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,6 +3,7 @@ from .users import router as users_router
 from .tasks import router as tasks_router
 from .configs import router as config_router
 from .transactions import router as transaction_router
+from .webhooks import router as webhooks_router
 
 router = APIRouter()
 
@@ -13,3 +14,5 @@ router.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
 router.include_router(config_router, prefix="/configs", tags=["configs"])
 
 router.include_router(transaction_router, prefix="/transactions", tags=["transactions"])
+
+router.include_router(webhooks_router, prefix="/webhooks", tags=["webhooks"])
